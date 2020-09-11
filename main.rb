c375b489eb0e8ae657100d212106d68eb0005423
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-10 20:06:48 -0500

    Created new players.

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
-puts "Hello World! "
+class Race
+
+    players = [{ name ="John" => position = 0, name ="Peter" => position = 0, name ="Charly" => position = 0}]
+
+    winner = nil
+
+    until !winner
+        players.each do |p|
+                dice = 1 + rand(6)
+                p[:position] += dice
+            if position >= 100
+                winner = p
+            break
+            end
+        end
+    end
+end
